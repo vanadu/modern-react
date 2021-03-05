bcc85948b7d6e4f43f80e7202e870988d4b7a420
unknown <van@vanocular.com>
2021-03-05 10:49:25 -0500

    Session 17 - 03.05.21

---
+++ store/modern-react-tutorial/complete/223 Mapping the Analogy to Redux/components/VideoItem.js
@@ -0,0 +1,19 @@
+import './VideoItem.css';
+import React from 'react';
+
+// !VA 127 props now contains the video array with all its data about the individual videos. So we can now destructure out the video array from props.
+const VideoItem = ({ video, onVideoSelect }) => {
+  // !VA 130 Now add the event handler for the click to get the selected video's detail. This requires an arrow function in the onClick prop. Otherwise, onVideoSelect would run, but it wouldn't find the video. I don't get this.
+  return (
+    <div onClick={ () => onVideoSelect(video) }className="video-item item">
+      <img className="ui image" alt={video.snippet.title }src={ video.snippet.thumbnails.medium.url} alt=""/>
+      <div className="content">
+        <div className="header">
+          {video.snippet.title}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoItem;
